8e5a4e062bd5c628e92ec742d46ef41364146672
Deepak-ugsot <deepak.kumar@upgradsot.com>
2026-06-27 19:54:11 +0530

    setup express

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
 import { Config } from './config/index.js';
+import app from './app.js';
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
 
-console.log(Config.PORT);
+startServer();
